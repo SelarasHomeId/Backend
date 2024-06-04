1f4221fe4d6ce4139d40a1f87e949e8974ac8d07
yusnarsetiyadi <yusnarsetiyadi150403@gmail.com>
2024-06-04 22:13:06 +0700

    rebuild #5

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"selarashomeid/pkg/constant"
 	"selarashomeid/pkg/util/validator"
@@ -16,11 +17,11 @@ func Init(e *echo.Echo) {
 	e.Use(Context)
 	e.Use(
 		echoMiddleware.Recover(),
-		// echoMiddleware.CORSWithConfig(echoMiddleware.CORSConfig{
-		// 	AllowOrigins: []string{"*"},
-		// 	AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, "x-user-id", "ngrok-skip-browser-warning", echo.HeaderAuthorization, echo.HeaderAccessControlAllowOrigin},
-		// 	AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete, http.MethodOptions, http.MethodHead, http.MethodPatch},
-		// }),
+		echoMiddleware.CORSWithConfig(echoMiddleware.CORSConfig{
+			AllowOrigins: []string{"*"},
+			AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, "x-user-id", "ngrok-skip-browser-warning", echo.HeaderAuthorization, echo.HeaderAccessControlAllowOrigin},
+			AllowMethods: []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete, http.MethodOptions, http.MethodHead, http.MethodPatch},
+		}),
 		echoMiddleware.LoggerWithConfig(echoMiddleware.LoggerConfig{
 			Format:           fmt.Sprintf("\n| %s | Host: ${host} | Time: ${time_custom} | Status: ${status} | LatencyHuman: ${latency_human} | UserAgent: ${user_agent} | RemoteIp: ${remote_ip} | Method: ${method} | Uri: ${uri} |\n", APP),
 			CustomTimeFormat: "2006/01/02 15:04:05",
